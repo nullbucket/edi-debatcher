d87096a000d2357c8a0df1b097b768ba5ac8b458
short000 <paul.short.software@gmail.com>
2018-03-22 15:44:45 -0500

    DebatcherTest passes 100%: fixes #4

--- src/test/java/org/null0/edi/debatcher/test/DebatcherTest.java
+++ src/test/java/org/null0/edi/debatcher/test/DebatcherTest.java
@@ -2,18 +2,13 @@ package org.null0.edi.debatcher.test;
 
 import java.io.InputStream;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.null0.edi.debatcher.Debatcher;
 
-// TODO: This is scary. All the useful tests have been commented out! Why?
-// First we'll get the other unit tests to pass (and verify their accuracy)
-// then we'll start enabling and verify these unit tests.
-//
 public class DebatcherTest {
 	private static final Logger logger = LoggerFactory.getLogger(DebatcherTest.class);
 	private static Debatcher debatcher;
@@ -34,122 +29,100 @@ public class DebatcherTest {
 	// value in Submission table. So, test cases will fail when there is no entry in
 	// submission table.
 
-	@org.junit.Test
-	public void test_00() {
-		try {
-			test ("BAD_FILE");
-		} catch (Exception e) {
-			Assert.fail(e.getMessage());
-		}
+	@Test(expected=NullPointerException.class)
+	public void test_00() throws Exception {
+		test ("BAD_FILE_DOES_NOT_EXIST");
 	}
 
-	// TODO: Begin to uncomment these tests and get them to work. This one reads past the last segment (IEA) and throws null pointer exception comparing IEA-02 to ISA-13.  
-	@org.junit.Test
+	@Test
 	public void test_01() throws Exception {
 		test ("1 ISA - 1 GS - 1 ST - 1 HL 20 - 1 HL 22 - 1 CLM");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_02() throws Exception {
 		test ("1 ISA - 1 GS - 1 ST - 1 HL 20 - 1 HL 22 - 2 CLMs");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_03() throws Exception {
 		test ("1 ISA - 1 GS - 1 ST - 1 HL 20 - 1 HL 22 - 3 CLMs");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_04() throws Exception {
 		test ("1 ISA - 1 GS - 1 ST - 1 HL 20 - 2 HL 22 - 1 CLM FOR EACH SUBSCRIBER");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_05() throws Exception {
 		test ("1 ISA - 1 GS - 1 ST - 1 HL 20 - 2 HL 22 - 2 CLMs FOR EACH SUBSCRIBER");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_06() throws Exception {
 		test ("1 ISA - 1 GS - 1 ST - 1 HL 20 - 1 HL 22 - 1 CLM - 1 HL 20 - 1 HL 22 - 1 CLM");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_07() throws Exception {
 		test ("1 ISA - 1 GS - 1 ST - 1 HL 20 - 1 HL 22 - 2 CLMs - 1 HL 20 - 1 HL 22 - 2 CLMs");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_08() throws Exception {
 		test ("1 ISA - 1 GS - 1 ST - 1 HL 20 - 1 HL 22 - 1 CLM - 1 ST - 1 HL 20 - 1 HL 22 - 1 CLM");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_09() throws Exception {
 		test ("1 ISA - 1 GS - 3 STs");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_10() throws Exception {
 		test ("1 ISA - 2 GSs");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_11() throws Exception {
 		test ("2 ISAs");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_12() throws Exception {
 		test ("837PFile_for de-batcher_multiple_fil_configs_inonefile");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_13() throws Exception {
 		test ("Patient");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_Large() throws Exception {
 		long startTime = System.currentTimeMillis();
-		test ("LargeP");
+		test ("837PFile_Large");
 		long endTime = System.currentTimeMillis();
 		logger.info("That took " + (endTime - startTime) + " milliseconds");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_P_NoRefD9() throws Exception {
 		test ("NoRefD9_P");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_P_RefD9() throws Exception {
 		test ("RefD9_P");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_I_NoRefD9() throws Exception {
 		test ("NoRefD9_I");
 	}
 
-	@Ignore
-	@org.junit.Test
+	@Test
 	public void test_I_RefD9() throws Exception {
 		test ("RefD9_I");
 	}
